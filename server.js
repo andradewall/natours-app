2c5e094069bde8a27ba6a1585ac8e027a9688a69
andradewall <andradewall0@gmail.com>
2022-03-07 12:58:37 -0300

    feat: handling unhandled rejections

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config({ path: './config.env' })
 
-const app = require('./app');
+const app = require('./app')
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-);
+)
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -15,9 +15,20 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => console.log('DB connection successful!'))
+  //.catch(err => console.error(err))
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`[app.js] App running on port ${port}`);
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+  console.log(`[server.js] Server running on port ${port}`)
 });
+
+process.on('unhandledRejection', err => {
+  console.error(err.name, err.message)
+  console.log('[server.js] ⚠️  Unhandled Rejection! Shutting down...')
+
+  // Finish all pending requests before kill
+  server.close(() => {
+    process.exit(1)
+  })
+})
