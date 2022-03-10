9106cfffbeeefe42e3645f0d891f0b1d9202cba8
andradewall <andradewall0@gmail.com>
2022-03-10 15:59:04 -0300

    feat: managing (hashging) passwords w/ bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require('express')
 
-const userController = require('./../controllers/userController');
+const userController = require('./../controllers/userController')
+const authController = require('./../controllers/authController')
 
 const router = express.Router();
+
+router.post('/signup', authController.signup)
+
 router
   .route('/')
   .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .post(userController.createUser)
 
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .delete(userController.deleteUser)
 
-module.exports = router;
+module.exports = router
