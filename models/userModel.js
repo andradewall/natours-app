73cd0c5a527db7729811bb72189a3a806cfdbcd4
andradewall <andradewall0@gmail.com>
2022-03-14 18:33:38 -0300

    feat: reset password

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -44,7 +45,9 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same'
     }
   },
-  passwordChangedAt: Date
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function(next) {
@@ -56,6 +59,14 @@ userSchema.pre('save', async function(next) {
 
   // Delete the passwordConfirm field
   this.passwordConfirm = undefined
+
+  next()
+})
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next()
+
+  this.passwordChangedAt = Date.now() - 1000
 
   next()
 })
@@ -77,6 +88,19 @@ userSchema.methods.changedPasswordAfterToken = function(jwtTimestamp) {
 
   // False means password NOT changed
   return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex')
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+
+  return resetToken
 }
 
 const User = mongoose.model('User', userSchema)
