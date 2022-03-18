4e420efe8d6191947d552ab6db20955e77844b1c
andradewall <andradewall0@gmail.com>
2022-03-18 11:44:10 -0300

    feat: importing users and reviews

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 require('dotenv').config({ path: './config.env' })
 const Tour = require('./../../models/tourModel')
+const User = require('./../../models/userModel')
+const Review = require('./../../models/reviewModel')
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -19,11 +21,17 @@ mongoose
 
 // Reading JSON file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+)
 
 // Importing data into database
 const importData = async () => {
   try {
     await Tour.create(tours)
+    await User.create(users, { validateBeforeSave: false })
+    await Review.create(reviews)
     console.log('Data successfully loaded!')
   } catch (err) {
     console.log('[error in import-dev-data.js]', err)
@@ -31,11 +39,12 @@ const importData = async () => {
   process.exit()
 }
 
-
 // Deleting all data from database
 const deleteData = async () => {
   try {
     await Tour.deleteMany()
+    await User.deleteMany()
+    await Review.deleteMany()
     console.log('Data successfully deleted!')
   } catch (err) {
     console.log('[error in import-dev-data.js]', err)
